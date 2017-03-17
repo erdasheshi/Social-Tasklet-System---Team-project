f3cd861068e454cf056a07763306848531c9e3e3
Sebastian F Jacobsen <sebastianjacobsen@hejsfj.com>
2017-03-17 13:44:33 +0100

    Merge Frontend & Backend

--- Teamproject/Frontend/src/app/register/register.component.ts
+++ Teamproject/Frontend/src/app/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+var conf = require('../../../config.json');
+var socket = require('socket.io-client')('http://localhost:' + conf.ports.sfbroker_socket);
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -15,10 +18,13 @@ export class RegisterComponent implements OnInit {
   clickMessage = '';
 
   onClickMe() {
-    var j={"userid":"test", "password":"test", "price":"test", "email":"test", "firstname":"test", "lastname":"test"};
+    var j={"type":"User", "userid":"test", "password":"123", "price":"1234", "email":"test", "firstname":"test", "lastname":"test"};
     JSON.stringify(j);
     this.clickMessage = 'You are my hero!';
     console.log(j);
+
+    socket.emit('SFWrite_User', j);
+
   }
 
 }
