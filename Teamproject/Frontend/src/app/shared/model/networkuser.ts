7a6129942b91a34784a36a8dcbc92f7413f702ef
Sebastian F Jacobsen <sebastianjacobsen@hejsfj.com>
2017-04-10 16:36:58 +0200

    Add shared models for network

---
+++ Teamproject/Frontend/src/app/shared/model/networkuser.ts
@@ -0,0 +1,10 @@
+export class NetworkUser {
+    constructor(
+        public id: string,
+        public username: string,
+        public firstname: string,
+        public lastname: string,
+        public email: string,
+        public price: number,
+        public balance: string) { }
+}
--- Teamproject/Frontend/src/app/shared/model/user.ts
+++ Teamproject/Frontend/src/app/shared/model/user.ts
@@ -1,3 +1,10 @@
-/**
- * Created by d060536 on 09/04/2017.
- */
+export class User {
+    constructor(
+
+        public username: string,
+        public firstname: string,
+        public lastname: string,
+        public email: string,
+        public price: number,
+        public password: string) { }
+}
