b0de0295d958bdf31d2c89d9644c000932c64b73
Philipp2501 <philipp.geissler1@gmx.de>
2017-04-22 16:07:05 +0200

    Frontend

--- Teamproject/Frontend3/src/app/shared/services/user.service.ts
+++ Teamproject/Frontend3/src/app/shared/services/user.service.ts
@@ -12,12 +12,12 @@ import {TransactionList} from '../model/transactionlist';
 @Injectable()
 export class UserService {
 
-    private apiURLRegister = 'http://ec2-52-11-156-237.us-west-2.compute.amazonaws.com:8001/register/';
-    private apiURLLogin = 'http://ec2-52-11-156-237.us-west-2.compute.amazonaws.com:8001/login/';
-    private apiURLNetwork = 'http://ec2-52-11-156-237.us-west-2.compute.amazonaws.com:8001/user/?all=X';
-    private apiURLFriendships = 'http://ec2-52-11-156-237.us-west-2.compute.amazonaws.com:8001/sfbuserinfo/';
-    private apiURLAddFriend = 'http://ec2-52-11-156-237.us-west-2.compute.amazonaws.com:8001/friendship/';
-    private apiURLTransaction = 'http://ec2-52-11-156-237.us-west-2.compute.amazonaws.com:8001/acctransaction';
+    private apiURLRegister = 'http://ec2-52-10-202-51.us-west-2.compute.amazonaws.com:8001/register/';
+    private apiURLLogin = 'http://ec2-52-10-202-51.us-west-2.compute.amazonaws.com:8001/login/';
+    private apiURLNetwork = 'http://ec2-52-10-202-51.us-west-2.compute.amazonaws.com:8001/user/?all=X';
+    private apiURLFriendships = 'http://ec2-52-10-202-51.us-west-2.compute.amazonaws.com:8001/sfbuserinfo/';
+    private apiURLAddFriend = 'http://ec2-52-10-202-51.us-west-2.compute.amazonaws.com:8001/friendship/';
+    private apiURLTransaction = 'http://ec2-52-10-202-51.us-west-2.compute.amazonaws.com:8001/acctransaction';
 
 
     constructor(private http: Http) {
