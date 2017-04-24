cb0de5be9f3c1ffb47a094a85b8b4537f0642049
Sebastian F Jacobsen <sebastianjacobsen@hejsfj.com>
2017-04-24 18:19:52 +0200

    Make use of config file and reset basic styles

--- Teamproject/Frontend/src/app/shared/services/user.service.ts
+++ Teamproject/Frontend/src/app/shared/services/user.service.ts
@@ -8,63 +8,63 @@ import {Friendship} from '../model/friendship';
 import {AddFriendship} from '../model/addFriendship';
 import {TransactionList} from '../model/transactionlist';
 
+var conf = require('../../../../../config.json');
 
 @Injectable()
 export class UserService {
 
-    private apiURLRegister = 'http://ec2-34-209-109-232.us-west-2.compute.amazonaws.com:8001/register/';
-    private apiURLLogin = 'http://ec2-34-209-109-232.us-west-2.compute.amazonaws.com:8001/login/';
-    private apiURLNetwork = 'http://ec2-34-209-109-232.us-west-2.compute.amazonaws.com:8001/user/?all=X';
-    private apiURLFriendships = 'http://ec2-34-209-109-232.us-west-2.compute.amazonaws.com:8001/sfbuserinfo/';
-    private apiURLAddFriend = 'http://ec2-34-209-109-232.us-west-2.compute.amazonaws.com:8001/friendship/';
-    private apiURLTransaction = 'http://ec2-34-209-109-232.us-west-2.compute.amazonaws.com:8001/acctransaction';
+  private apiURLRegister = conf.sfbroker.ip + ':8001/register/';
+  private apiURLLogin = conf.sfbroker.ip + ':8001/login/';
+  private apiURLNetwork = conf.sfbroker.ip + ':8001/user/?all=X';
+  private apiURLFriendships = conf.sfbroker.ip + ':8001/sfbuserinfo/';
+  private apiURLAddFriend = conf.sfbroker.ip + ':8001/friendship/';
+  private apiURLTransaction = conf.sfbroker.ip + ':8001/acctransaction';
+
+  constructor(private http: Http) {
+  }
+
+  registerUser(newUser: User): Promise<any> {
+    return this.http.post(this.apiURLRegister, newUser)
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  loginUser(newUser: User): Promise<any> {
+    return this.http.post(this.apiURLLogin, newUser)
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  getNetwork(): Promise<NetworkUser[]> {
+    return this.http.get(this.apiURLNetwork)
+      .toPromise()
+      .then((res: Response) => res.json()[0].map(obj => new NetworkUser(obj)))
+      .catch(this.handleError);
+  }
+
+  getTransactions(): Promise<TransactionList[]> {
+    return this.http.get(this.apiURLTransaction)
+      .toPromise()
+      .then((res: Response) => res.json().map(obj => new TransactionList(obj)))
+      .catch(this.handleError);
+  }
+
+  getFriends(): Promise<Friendship[]> {
+    return this.http.get(this.apiURLFriendships)
+      .toPromise()
+      .then((res: Response) => res.json().connections.map(obj => new Friendship(obj)))
+      .catch(this.handleError);
+  }
+
+  addFriend(addFriendship: AddFriendship): Promise<any> {
+    return this.http.post(this.apiURLAddFriend, addFriendship)
+      .toPromise()
+      .catch(this.handleError);
+  }
 
 
-    constructor(private http: Http) {
-    }
-
-    registerUser(newUser: User): Promise<any> {
-        return this.http.post(this.apiURLRegister, newUser)
-            .toPromise()
-            .catch(this.handleError);
-    }
-
-    loginUser(newUser: User): Promise<any> {
-        return this.http.post(this.apiURLLogin, newUser)
-            .toPromise()
-            .catch(this.handleError);
-    }
-
-    getNetwork(): Promise<NetworkUser[]> {
-        return this.http.get(this.apiURLNetwork)
-            .toPromise()
-            .then((res: Response) => res.json()[0].map(obj => new NetworkUser(obj)))
-            .catch(this.handleError);
-    }
-
-    getTransactions(): Promise<TransactionList[]> {
-        return this.http.get(this.apiURLTransaction)
-            .toPromise()
-            .then((res: Response) => res.json().map(obj => new TransactionList(obj)))
-            .catch(this.handleError);
-    }
-
-    getFriends(): Promise<Friendship[]> {
-        return this.http.get(this.apiURLFriendships)
-            .toPromise()
-            .then((res: Response) => res.json().connections.map(obj => new Friendship(obj)))
-            .catch(this.handleError);
-    }
-
-    addFriend(addFriendship: AddFriendship): Promise<any> {
-        return this.http.post(this.apiURLAddFriend, addFriendship)
-            .toPromise()
-            .catch(this.handleError);
-    }
-
-
-    private handleError(error: any): Promise<any> {
-        return Promise.reject(error.message || error);
-    }
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
+  }
 
 }
--- Teamproject/Frontend/src/app/transactions/transactions.component.ts
+++ Teamproject/Frontend/src/app/transactions/transactions.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Transaction } from './transaction';
 import { UserService } from '../shared/services/user.service'; //API Service
 import { TransactionList } from '../shared/model/transactionlist';
 
 var conf = require('../../../config.json');
-var socket = require('socket.io-client')('http://localhost:' + conf.ports.sfbroker_socket);
 
 @Component({
   selector: 'app-transactions',
@@ -19,7 +17,6 @@ export class TransactionsComponent implements OnInit {
   constructor(private userService: UserService,) { }
 
   ngOnInit() {
-    //socket.emit('SFRead_Acc')
 
     //get all transactions
     this.userService
@@ -41,14 +38,5 @@ export class TransactionsComponent implements OnInit {
   getTransactions(): TransactionList[] {
       return this.transactionListItems;
   }
-
-  transactions = [
-    new Transaction('Sebastian', 'Sammer', 250, 250, 'successful', '1'),
-    new Transaction('Sebastian', 'Erda', 450, 450, 'successful', '1'),
-    new Transaction('Sebastian', 'Alex', 450, 450, 'successful', '1'),
-    new Transaction('Sebastian', 'Sammer', 750, 750, 'successful', '1'),
-    new Transaction('Sebastian', 'Sammer', 1850, 1850, 'successful', '1'),
-    new Transaction('Sebastian', 'Sammer', 50, 50, 'successful', '1')
-  ];
 
 }
