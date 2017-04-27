8c64d01c534bb52dc7fe04013fd2630fcc225342
Alex0815 <alex.b0815@gmx.de>
2017-04-27 11:11:36 +0200

    Fix Update Friendship

--- Teamproject/SFBroker/classes/FriendshipTransaction.js
+++ Teamproject/SFBroker/classes/FriendshipTransaction.js
@@ -1,12 +1,13 @@
-var constants = require('../constants');
-var Models = require("../app"); //Instantiate a Models object so you can access the models.js module.
+var constants   = require('../constants');
+var Models      = require("../app"); //Instantiate a Models object so you can access the models.js module.
 
-var mongoose = require('mongoose');
+var mongoose    = require('mongoose');
 var Friendships = require("../models/Friendships");
+var dbAccess    = require("../routes/dbAccess");
 
-var Friendship = mongoose.model("Friendship", Friendships.friendshipSchema);
+var Friendship  = mongoose.model("Friendship", Friendships.friendshipSchema);
 
-module.exports = friendshipTransaction
+module.exports  = friendshipTransaction
 
 function friendshipTransaction(data, callback) {
     this.user_1 = data.user_1;
@@ -20,17 +21,30 @@ friendshipTransaction.prototype.save =  function(callback) {
     var status = this.status;
     Friendship.findOne({ 'user_1' : this.user_1 , 'user_2' : this.user_2 }).exec(function (e, udata) {
         if(udata == null){
-            var transaction = new Friendship({ //You're entering a new transaction here
-                user_1: user_1,
-                user_2: user_2,
-                status: status
-            });
-            transaction.save({}, function (error, data) {
-                if(error){
-                    callback(error, false);
+            Friendship.findOne({ 'user_1' : user_2 , 'user_2' : user_1 }).exec(function (e, data) {
+                if(data == null){
+                    var transaction = new Friendship({ //You're entering a new transaction here
+                        user_1: user_1,
+                        user_2: user_2,
+                        status: status
+                    });
+                    transaction.save({}, function (error, data) {
+                        if(error){
+                            callback(error, false);
+                        }
+                        if(callback) callback(null, true);
+                    });
                 }
-                if(callback) callback(null, true);
+                else {
+                    Friendship.update({'user_1' : user_2 , 'user_2' : user_1}, {'status': status}, function(e, data){
+                        if(e){
+                            callback(e, false);
+                        }
+                        if(callback) callback(null, true);
+                    });
+                }
             });
+
         }
         else{
             Friendship.update({'user_1' : user_1 , 'user_2' : user_2}, {'status': status}, function(e, data){
