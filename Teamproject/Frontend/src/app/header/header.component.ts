b8153ab58b66e0539fef6a90c5abced9d7a6344c
Sebastian F Jacobsen <sebastianjacobsen@hejsfj.com>
2017-07-12 15:04:07 +0200

    Squashed commit of the following:
    commit eb38daedf48d023387c4a2c7de246f834c6bfe2d
    Author: Sebastian F Jacobsen <sebastianjacobsen@hejsfj.com>
    Date:   Wed Jul 12 15:02:29 2017 +0200
    
        Change Server URL in Service
    
    commit 4a310a9464f35aea549eb98de834ed92970b7dbd
    Author: Sebastian F Jacobsen <sebastianjacobsen@hejsfj.com>
    Date:   Wed Jul 12 15:01:12 2017 +0200
    
        Add comments, Add Server URL
    
    commit 056ab2a5c5c04fa618cedd896ff33fcd6a30a17a
    Author: Philipp2501 <philipp.geissler1@gmx.de>
    Date:   Wed Jul 12 11:44:19 2017 +0200
    
        Logout
    
    commit 0efc704c9ccaf6f4fb9924f1f3a6bb547e74cbe0
    Author: Philipp2501 <philipp.geissler1@gmx.de>
    Date:   Wed Jul 12 10:38:33 2017 +0200
    
        Real-Time Reload of Coins Request History List
    
    commit a66a5f87e909135bca6ba8a70511d37e0b4a8a6f
    Author: Philipp2501 <philipp.geissler1@gmx.de>
    Date:   Tue Jul 11 23:53:04 2017 +0200
    
        Requested Coins List
    
    commit 2950fbe9e5e2e0bee13a25719810d86554aab60f
    Author: Philipp2501 <philipp.geissler1@gmx.de>
    Date:   Tue Jul 11 19:01:52 2017 +0200
    
        Implementation of Requested Coins List
    
    commit e17f940996cde6b53d7d9cd9696f440397cb90fa
    Author: Philipp2501 <philipp.geissler1@gmx.de>
    Date:   Tue Jul 11 18:06:02 2017 +0200
    
        Revert "Add Admin to Docker more and more :-)"
    
        This reverts commit b2cd6ce18cb32c75b4b5cbc6ff668865d6d2e062.
    
    commit b2cd6ce18cb32c75b4b5cbc6ff668865d6d2e062
    Author: Philipp2501 <philipp.geissler1@gmx.de>
    Date:   Tue Jul 11 18:05:18 2017 +0200
    
        Add Admin to Docker more and more :-)
    
    commit dde953e947d98cd11682c63e794d27743bb66955
    Author: Philipp2501 <philipp.geissler1@gmx.de>
    Date:   Tue Jul 11 17:45:33 2017 +0200
    
        Change initial balance to 0
    
    commit 832b012cbaaa7f0d75f208c9f6d45341991d6fb7
    Author: Sebastian F Jacobsen <sebastianjacobsen@hejsfj.com>
    Date:   Tue Jul 11 15:07:36 2017 +0200
    
        Squashed commit of the following:
    
        commit 16bb6f85d3526915775e5cc9f5cae14d0505c2d2
        Author: Alex0815 <alex.b0815@gmx.de>
        Date:   Fri Jul 7 12:37:27 2017 +0200
    
            Fix Login bug and enable cors

--- Teamproject/Frontend/src/app/header/header.component.ts
+++ Teamproject/Frontend/src/app/header/header.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/services/user.service'; //API Service
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css', '../shared/styles/grid.css', '../shared/styles/global.css']
+  styleUrls: ['./header.component.css', '../shared/styles/grid.css', '../shared/styles/global.css'],
+  providers: [UserService] //API Service
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
       }
 
+  logoutFunction(){
+
+    //logout
+    this.userService
+      .logoutUser()
+      .then(result => {
+        console.log('Tscchüüüsssiii');
+      })
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
+  }
 }
